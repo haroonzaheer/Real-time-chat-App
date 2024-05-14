4ac2bf9fc56ba7f3d362a870ddd99b43f027d404
haroonzaheer <haroonzaheer04@gmail.com>
2024-05-14 20:54:46 +0500

    Create Express Server

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require("express")
+const cors = require("cors")
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+const port=5000;
+
+app.listen(port, (req, res) =>{
+    console.log(`Server is running on: ${port}`);
+});
