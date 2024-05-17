6abe4ef61dc7c56fb4cb754a4e9c88ce033aef84
haroonzaheer <haroonzaheer04@gmail.com>
2024-05-17 08:21:54 +0500

    connection extablished

--- server/index.js
+++ server/index.js
@@ -1,27 +1,3 @@
-// const express = require("express")
-// const cors = require("cors")
-// const mongoose = require("mongoose")
-
-// const app = express()
-// require("dotenv").config()
-
-// app.use(express.json());
-// app.use(cors());
-
-// const port = process.env.PORT || 5000;
-// const uri = process.env.ATLAS_URI;
-
-
-// app.listen(port, (req, res) =>{
-//     console.log(`Server is running on: ${port}`);
-// });
-
-// mongoose.connect(uri, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-// })
-// .then(() => console.log("MongooseDB connection extablished"))
-// .catch((error) => console.log("MongoDb connection failed: " , error.message));
 
 const express = require("express");
 const cors = require("cors");
